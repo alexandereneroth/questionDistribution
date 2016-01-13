2ede2ee0166369fb8c325b19c6fe25353034b631
alexander-eneroth <alexander.eneroth@apegroup.com>
2016-01-13 11:23:18 +0100

    Simplifies testcases

--- src/se/alen/labs/questiondistribution/ImplementationTest.java
+++ src/se/alen/labs/questiondistribution/ImplementationTest.java
@@ -1,16 +1,13 @@
 package se.alen.labs.questiondistribution;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.*;
 
 public class ImplementationTest {
 
@@ -30,26 +27,14 @@ public class ImplementationTest {
     List<Implementation.RecipeQuestion> recipeQuestions = new ArrayList<>();
     List<Implementation.CuratedQuestion> curatedQuestions = new ArrayList<>();
 
-    @Before
-    public void setUp() {
-        recipeQuestions = new ArrayList<>();
-        curatedQuestions = new ArrayList<>();
-    }
-
     @Test
     public void shouldCorrectlyDistribute_7CQ_NoInteractiveQuestion() throws Exception {
 
-        curatedQuestions = newArrayList(cq1,cq2,cq3,cq4,cq5,cq6,cq7);
+        curatedQuestions = newArrayList(cq1, cq2, cq3, cq4, cq5, cq6, cq7);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(null, recipeQuestions, curatedQuestions);
 
-        assertEquals(cq1, questions.get(0));
-        assertEquals(cq2, questions.get(1));
-        assertEquals(cq3, questions.get(2));
-        assertEquals(cq4, questions.get(3));
-        assertEquals(cq5, questions.get(4));
-        assertEquals(cq6, questions.get(5));
-        assertEquals(cq7, questions.get(6));
+        assertEquals(newArrayList(cq1, cq2, cq3, cq4, cq5, cq6, cq7), questions);
     }
 
     @Test
@@ -60,10 +45,7 @@ public class ImplementationTest {
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(null, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_2RQ_6CQ_NoInteractiveQuestion");
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_2RQ_6CQ_NoInteractiveQuestion", questions);
 
         assertTrue("Wrongly distributed list", Arrays.asList(cq1, cq2, rq1, cq3, cq4, cq5, rq2, cq6).equals(questions)
                         || Arrays.asList(cq1, cq2, cq3, rq1, cq4, cq5, rq2, cq6).equals(questions)
@@ -73,88 +55,50 @@ public class ImplementationTest {
     @Test
     public void shouldCorrectlyDistribute_2CQ() throws Exception {
 
-        curatedQuestions.add(cq1);
-        curatedQuestions.add(cq2);
+        curatedQuestions = newArrayList(cq1, cq2);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(interactive, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_2CQ");
-        // Should print: I, CQ1, CQ2
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_2CQ", questions);
 
-        assertEquals(interactive, questions.get(0));
-        assertEquals(cq1, questions.get(1));
-        assertEquals(cq2, questions.get(2));
+        assertEquals(newArrayList(interactive, cq1, cq2), questions);
     }
 
     @Test
     public void shouldCorrectlyDistribute_1RQ_3CQ() throws Exception {
 
-        recipeQuestions.add(rq1);
-        curatedQuestions.add(cq1);
-        curatedQuestions.add(cq2);
-        curatedQuestions.add(cq3);
+        recipeQuestions = newArrayList(rq1);
+        curatedQuestions = newArrayList(cq1, cq2, cq3);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(interactive, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_1RQ_3CQ");
-        // Should print: I, CQ1, RQ1, CQ2, CQ3
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_1RQ_3CQ", questions);
 
-        assertEquals(interactive, questions.get(0));
-        assertEquals(cq1, questions.get(1));
-        assertEquals(cq2, questions.get(2));
-        assertEquals(rq1, questions.get(3));
-        assertEquals(cq3, questions.get(4));
+        assertEquals(newArrayList(interactive, cq1, cq2, rq1, cq3), questions);
     }
 
     @Test
     public void shouldCorrectlyDistribute_4RQ_5CQ() throws Exception {
 
-        recipeQuestions = Arrays.asList(rq1,rq2,rq3,rq4);
-        curatedQuestions = Arrays.asList(cq1,cq2,cq3,cq4,cq5);
+        recipeQuestions = Arrays.asList(rq1, rq2, rq3, rq4);
+        curatedQuestions = Arrays.asList(cq1, cq2, cq3, cq4, cq5);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(interactive, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_4RQ_6CQ");
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_4RQ_6CQ", questions);
 
-        assertEquals(interactive, questions.get(0));
-        assertEquals(cq1, questions.get(1));
-        assertEquals(rq1, questions.get(2));
-        assertEquals(cq2, questions.get(3));
-        assertEquals(rq2, questions.get(4));
-        assertEquals(cq3, questions.get(5));
-        assertEquals(rq3, questions.get(6));
-        assertEquals(cq4, questions.get(7));
-        assertEquals(rq4, questions.get(8));
-        assertEquals(cq5, questions.get(9));
+        assertEquals(newArrayList(interactive, cq1, rq1, cq2, rq2, cq3, rq3, cq4, rq4, cq5), questions);
     }
 
     @Test
     public void shouldCorrectlyDistribute_2RQ_6CQ() throws Exception {
 
-        recipeQuestions.add(rq1);
-        recipeQuestions.add(rq2);
-        curatedQuestions.add(cq1);
-        curatedQuestions.add(cq2);
-        curatedQuestions.add(cq3);
-        curatedQuestions.add(cq4);
-        curatedQuestions.add(cq5);
-        curatedQuestions.add(cq6);
+        recipeQuestions = newArrayList(rq1, rq2);
+        curatedQuestions = newArrayList(cq1, cq2, cq3, cq4, cq5, cq6);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(interactive, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_2RQ_6CQ");
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_2RQ_6CQ", questions);
 
         assertEquals(interactive, questions.get(0));
         assertEquals(cq1, questions.get(1));
@@ -176,18 +120,12 @@ public class ImplementationTest {
 
     @Test
     public void shouldCorrectlyDistribute_3RQ_2CQ() throws Exception {
-        recipeQuestions.add(rq1);
-        recipeQuestions.add(rq2);
-        recipeQuestions.add(rq3);
-        curatedQuestions.add(cq1);
-        curatedQuestions.add(cq2);
+        recipeQuestions = newArrayList(rq1,rq2,rq3);
+        curatedQuestions = newArrayList(cq1,cq2);
 
         final List<Implementation.Question> questions = Implementation.distributeQuestionsInList(interactive, recipeQuestions, curatedQuestions);
 
-        System.out.println("\nshouldCorrectlyDistribute_3RQ_2CQ");
-        for (Implementation.Question q : questions) {
-            System.out.println(q.getName());
-        }
+        printQuestions("shouldCorrectlyDistribute_3RQ_2CQ", questions);
 
         assertEquals(interactive, questions.get(0));
 
@@ -208,5 +146,12 @@ public class ImplementationTest {
         }
 
         assertEquals(cq2, questions.get(5));
+    }
+
+    private void printQuestions(String heading, List<Implementation.Question> questions){
+        System.out.println("\n" + heading);
+        for (Implementation.Question q : questions) {
+            System.out.println(q.getName());
+        }
     }
 }
